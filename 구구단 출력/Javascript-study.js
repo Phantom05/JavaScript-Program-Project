21a3d5c0587afecebfa3c72246d64d90386ce225
Phantom05 <monster2jy@gmail.com>
2018-06-08 15:16:45 +0900

    docs:Edit Javascript-study.js New 99dan

--- 구구단 출력/Javascript-study.js
+++ 구구단 출력/Javascript-study.js
@@ -1,26 +1,38 @@
- //스코프체이닝 + 재귀함수 를 이용한 구구단 출력  
+//스코프체이닝 + 재귀함수 를 이용한 구구단 출력  
 
- (function () {
-     let parent = 2;
-     closure()
+//구구단
+let goo = { start: 2, con: 1 }
+dan = () => {
+  console.log(`${goo.start}x${goo.con}=${goo.start * goo.con}`);
+  goo.con += 1;
+  (goo.con < 10) ? dan() : goo.con = 1;
+}
+count = () => {
+  for (; goo.start < 10; goo.start += 1) {
+    console.log(`${goo.start}단`);
+    dan()
+  }
+}
+(goo.start == 2) ? count() : null;
 
-     function closure() {
+(function () {
+  let parent = 2;
+  closure()
+  function closure() {
+    if (parent < 10) {
+      console.log(parent + '단 시작합니다.');
+      let son = 0
+      ser();
+      parent++;
+      closure();
+      function ser() {
+        if (son < 9) {
+          son++;
+          console.log(parent + '*' + son + "=" + parent * son);
+          ser();
+        }
+      }
 
-         if (parent < 10) {
-             console.log(parent + '단 시작합니다.');
-             let son = 0
-             ser();
-             parent++;
-             closure();
-
-             function ser() {
-                 if (son < 9) {
-                     son++;
-                     console.log(parent + '*' + son + "=" + parent * son);
-                     ser();
-                 }
-             }
-
-         }
-     }
- })()
+    }
+  }
+})()
